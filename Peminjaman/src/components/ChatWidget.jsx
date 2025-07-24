516db56ce0fe20f544ea63315fd569901e92ca41
Ryn104 <riyananggacuy@gmail.com>
2025-07-24 16:12:26 +0700

    fix design chat admin

--- Peminjaman/src/components/ChatWidget.jsx
+++ Peminjaman/src/components/ChatWidget.jsx
@@ -90,7 +90,7 @@ export default function ChatWidget() {
                 {!isOpen && (
                     <button
                         onClick={toggleChat}
-                        className="btn btn-circle w-14 h-14 lg:w-14 lg:h-14 bg-green-500 hover:bg-green-600 border border-gray-200 text-white shadow-xl"
+                        className="btn btn-circle w-14 h-14 lg:w-14 lg:h-14 bg-[#E60000] hover:bg-[#b80000] border border-gray-200 text-white shadow-xl"
                     >
                         <FaWhatsapp className="text-2xl lg:text-3xl" />
                     </button>
@@ -98,67 +98,75 @@ export default function ChatWidget() {
 
                 {/* Chat box */}
                 {isOpen && (
-                    <div className="relative mt-2 shadow-xl bg-white border border-gray-200 rounded-2xl p-6 w-65 lg:w-80">
+                    <div className="relative mt-2 shadow-xl bg-white border border-gray-200 rounded-2xl px-4 pt-2 pb-3 lg:py-4 lg:px-6 w-65 lg:w-98">
                         {/* Tombol Close (X) */}
-                        <button
-                            onClick={toggleChat}
-                            className="absolute top-2 right-2 text-gray-500 hover:text-red-500 text-xl"
-                            aria-label="Tutup"
-                        >
-                            <IoClose />
-                        </button>
-
-                        <h3 className="text-lg lg:text-xl text-center text-[#002B5B] font-semibold mb-3 lg:mb-6">Hubungi Admin</h3>
+                        <div className="absolute top-0 left-0 w-full h-1/2 bg-[#D62828] rounded-t-2xl rounded-bl-[30%] z-0"></div>
+                        <div className="relative z-10 p-4 text-white">
+                            <button
+                                onClick={toggleChat}
+                                className="absolute top-2 right-2 text-white hover:text-red-300 text-xl lg:text-2xl"
+                                aria-label="Tutup"
+                            >
+                                <IoClose />
+                            </button>
+                            <h3 className="text-lg lg:text-2xl text-center text-white font-semibold lg:mb-5">Hubungi Admin</h3>
+                        </div>
                         <form onSubmit={handleSubmitChat} className="flex flex-col gap-2 lg:gap-3">
-                            <input
-                                type="text"
-                                name="nama"
-                                placeholder="Nama"
-                                className="input input-bordered h-8 lg:h-10 lg:text-md"
-                                value={form.nama}
-                                onChange={handleChange}
-                                required
-                            />
-                            <input
-                                type="number"
-                                name="nomor"
-                                placeholder="Nomor"
-                                className="input input-bordered h-8 lg:h-10 lg:text-md"
-                                value={form.nomor}
-                                onChange={handleChange}
-                                required
-                            />
-                            <input
-                                type="text"
-                                name="divisi"
-                                placeholder="Divisi"
-                                className="input input-bordered h-8 lg:h-10 lg:text-md"
-                                value={form.divisi}
-                                onChange={handleChange}
-                                required
-                            />
-                            <textarea
-                                name="pesan"
-                                rows="3"
-                                className="textarea textarea-bordered lg:text-md"
-                                placeholder="Ketik pesan..."
-                                value={form.pesan}
-                                onChange={handleChange}
-                                required
-                            ></textarea>
+                            <div className='bg-white rounded-lg p-3 flex flex-col z-10 border border-gray-200'>
+                                <label className="label text-sm">Nama</label>
+                                <input
+                                    type="text"
+                                    name="nama"
+                                    placeholder="Masukan Nama"
+                                    className="input input-bordered h-8 lg:h-10 lg:text-md mb-3"
+                                    value={form.nama}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label className="label text-sm">Nomor</label>
+                                <input
+                                    type="number"
+                                    name="nomor"
+                                    placeholder="Masukan Nomor"
+                                    className="input input-bordered h-8 lg:h-10 lg:text-md mb-3"
+                                    value={form.nomor}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label className="label text-sm">Unit/Divisi</label>
+                                <input
+                                    type="text"
+                                    name="divisi"
+                                    placeholder="Masukan Divisi"
+                                    className="input input-bordered h-8 lg:h-10 lg:text-md mb-3"
+                                    value={form.divisi}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label className="label text-sm">Pesan</label>
+                                <textarea
+                                    name="pesan"
+                                    rows="3"
+                                    className="textarea textarea-bordered lg:text-md"
+                                    placeholder="Ketik pesan..."
+                                    value={form.pesan}
+                                    onChange={handleChange}
+                                    required
+                                ></textarea>
+                            </div>
                             <button
                                 type="submit"
-                                className="btn bg-[#E60000] hover:bg-[#b80000] text-white border-none mt-2 lg:mt-4 h-8 lg:h-10 lg:text-md"
+                                className="btn bg-[#E60000] hover:bg-[#b80000] text-white border-none mt-2 lg:mt-4 h-8 lg:h-10 lg:text-md shadow-none mb-2 lg:mb-4"
                                 disabled={loading}
                             >
                                 {loading ? 'Mengirim...' : 'Kirim'}
                             </button>
 
-                            <div className='text-center'>
-                                <div className='w-full h-0.5 border-t border-gray-300 mb-1'></div>
-                                <span className='text-xs lg:text-sm text-center text-gray-500'>Hubungi Langsung <a href="https://wa.me/6285711002971">+6285711002971</a></span>
-                            </div>
                         </form>
+                        <div className='text-center'>
+                            <div className='w-full h-0.5 border-t border-gray-300 mb-1'></div>
+                            <span className='text-xs lg:text-sm text-center text-gray-500'>Hubungi Langsung <a href="https://wa.me/6285711002971" className='hover:text-red-500 font-medium'>+6285711002971</a></span>
+                        </div>
                     </div>
                 )}
 
